1f79216cca5b4f4cd38ccb2f92ecfacde075c622
Sergio444757 <9028570@student.zadkine.nl>
2026-03-06 10:57:19 +0100

    Add files via upload

---
+++ opdracht_4/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$games = [
+[
+"titel" => "Fortnite",
+"genre" => "Battle Royale",
+"platform" => "PS5"
+],
+[
+"titel" => "Minecraft",
+"genre" => "Sandbox",
+"platform" => "PC&PS"
+]
+];
+
+?>
+
+<html>
+<head>
+<title>Games</title>
+</head>
+
+<body>
