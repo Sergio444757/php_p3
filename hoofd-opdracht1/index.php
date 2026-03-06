359a9cad7af7cbf7029bd3c74259e0707434fb51
Sergio444757 <9028570@student.zadkine.nl>
2026-03-06 10:46:11 +0100

    Add files via upload

---
+++ hoofd-opdracht1/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// Mijn mini-app wordt een: Workout tracker
+
+$appNaam = "FitApp";
+$trackerType = "Workout tracker";
+$tagline = "Track je workouts";
+
+?>
+
+<html>
+<head>
+<title><?php echo $appNaam; ?></title>
+</head>
+
+<body>
+
+<h1>Welkom bij <?php echo $appNaam; ?></h1>
+
+<p>Dit is een <?php echo $trackerType; ?></p>
+
+<p><?php echo $tagline; ?></p>
+
+<footer>
+<?php echo date("Y"); ?> <?php echo $appNaam; ?>
+</footer>
+
+</body>
+</html>
+
