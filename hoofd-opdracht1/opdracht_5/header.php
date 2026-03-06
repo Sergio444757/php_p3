1ac74045ebd36eebcedff38df9b02f40a13cf602
Sergio444757 <9028570@student.zadkine.nl>
2026-03-06 11:16:23 +0100

    Add files via upload

---
+++ hoofd-opdracht1/opdracht_5/header.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welkom bij mijn app</h1>
+<p>Dit is de rest van mijn pagina.</p>
+
+</body>
+</html>
